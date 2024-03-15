03c8bc2fa4ec01d4cfad8fff5d4f97a18cd18d7f
THIAGO DANTAS DIAS <109814439+Saydantas@users.noreply.github.com>
2024-03-15 12:28:33 -0300

    Update 219-Validação de Notas.c

--- 219-Validação de Notas.c
+++ 219-Validação de Notas.c
@@ -14,3 +14,35 @@ Quando duas notas válidas forem lidas, deve ser impressa a mensagem "media =
 o ponto decimal.*/
 
 
+#include <stdio.h>
+
+int main() {
+    double nota1, nota2;
+    int notas_validas = 0;
+    double notas[2];
+
+    while (notas_validas < 2) {
+        scanf("%lf", &nota1);
+        if (nota1 >= 0 && nota1 <= 10) {
+            notas[notas_validas] = nota1;
+            notas_validas++;
+        } else {
+            printf("nota invalida\n");
+        }
+
+        if (notas_validas < 2) {
+            scanf("%lf", &nota2);
+            if (nota2 >= 0 && nota2 <= 10) {
+                notas[notas_validas] = nota2;
+                notas_validas++;
+            } else {
+                printf("nota invalida\n");
+            }
+        }
+    }
+
+    double media = (notas[0] + notas[1]) / 2.0;
+    printf("media = %.2lf\n", media);
+
+    return 0;
+}

f50ac33836680620302b522fa7069298a09a88ed
THIAGO DANTAS DIAS <109814439+Saydantas@users.noreply.github.com>
2024-03-15 13:12:56 -0300

    Create 229-Bob Conduite.c

---
+++ 229-Bob Conduite.c
@@ -0,0 +1,35 @@
+/*Você tem em mãos dois cabos circulares de energia. O primeiro cabo tem raio R1 
+e o segundo raio R2. Você precisa comprar um conduite circular (veja a imagem 
+abaixo que ilustra um conduite) de maneira a passar os dois cabos por dentro dele:
+
+Qual o menor raio do conduite que você deve comprar? Em outras palavras, dado dois 
+círculos, qual o raio do menor círculo que possa englobar ambos os dois?
+
+Entrada
+Na primeira linha teremos um inteiro T (T ≤ 10000), indicando o número de casos de teste.
+
+Na única linha de cada caso teremos dois números inteiros R1 e R2, indicando os 
+respectivos raios. Os inteiros serão positivos e todas as contas caberão em um 
+inteiro normal de 32 bits.
+Saída
+Em cada caso, imprima o menor raio possível em uma única linha*/
+
+
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T); // Ler o número de casos de teste
+    
+    while (T--) {
+        int R1, R2;
+        scanf("%d %d", &R1, &R2); // Ler os raios dos cabos
+        
+        // Calcular o menor raio possível do conduite
+        int menor_raio = R1 + R2;
+        
+        printf("%d\n", menor_raio);
+    }
+    
+    return 0;
+}
